37c887acc700f0dffb6b10bbd0035de98824e190
marrgarret <margarita.zhev@mail.ru>
2021-07-10 11:59:52 +0300

    qa

---
+++ QA/src/test/java/org/example/LoginTest.java
@@ -0,0 +1,59 @@
+package org.example;
+
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class LoginTest {
+    public static LoginPage loginPage;
+    public static ProfilePage profilePage;
+
+    public static WebDriver driver;
+
+    @BeforeClass()
+    public static void setup() {
+        // определние пути до драйвера и его настройка
+        System.setProperty("webdriver.chrome.driver", "c:\\webdrivers\\chromedriver.exe");
+        // создание экземпляра драйвера
+        driver = new ChromeDriver();
+        loginPage = new LoginPage(driver);
+        profilePage = new ProfilePage(driver);
+        // окно разворачивается на полный экран
+        driver.manage().window().maximize();
+        // задержка на выполнение теста 10 секунд
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        // получение ссылки на страницу входа из файла настроек
+        driver.get(ConfProperties.getProperty("loginpage"));
+    }
+
+    @Test
+    public void loginTest() {
+        //значение login/password берутся из файла настроек по аналогии с chromedriver
+        //и loginpage
+
+        // вводим логин
+        loginPage.inputLogin(ConfProperties.getProperty("login"));
+        // нажимаем кнопку входа
+        loginPage.clickLoginBtn();
+        // вводим пароль
+        loginPage.inputPasswd(ConfProperties.getProperty("password"));
+        // нажимаем кнопку входа
+        loginPage.clickLoginBtn();
+        // получаем отображаемый логин
+        String user = profilePage.getUserName();
+        // и сравниваем его с логином из файла настроек
+        Assert.assertEquals(ConfProperties.getProperty("login"), user);
+    }
+
+    @AfterClass
+    public static void tearDown() {
+        profilePage.entryMenu();
+        profilePage.userLogout();
+        driver.quit();
+    }
+}
